7974b26d95e6b71642782f0cb5c8542262c41308
egwuenu gift <laurandidi21@gmail.com>
2017-09-23 23:54:49 +0100

    ghpages setup UI redesign

--- script.js
+++ script.js
@@ -1,45 +1,70 @@
-var colors = generateRandomColors(6);
+var numSquares = 6
+var colors = generateRandomColors(numSquares);
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easy = document.querySelector("#easyBtn");
+var hard = document.querySelector("#hardBtn");
 
-    var squares = document.querySelectorAll(".square");
-    var pickedColor = pickColor();
-    var colorDisplay = document.getElementById("colorDisplay");
-    var messageDisplay = document.querySelector("#message");
-    var h1 = document.querySelector("h1");
-    var resetButton = document.querySelector("#reset");
-
-
-    resetButton.addEventListener("click", function(){
-        // generate all new colors
-        colors = generateRandomColors(6);
-        // pick a new random color from array
+    easyBtn.addEventListener("click", function(){
+        hardBtn.classList.remove("selected");
+        easyBtn.classList.add("selected");
+        numSquares = 3;
+        colors = generateRandomColors(numSquares);
         pickedColor = pickColor();
-        // change color display to match picked color
         colorDisplay.textContent = pickedColor;
-        // change coklors of squares
-        for(var i = 0;  i < squares.length; i++){
-            squares[i].style.background = colors[i]; 
+        for(var i = 0; i < squares.length; i++){
+            if(colors[i]){
+                squares[i].style.background = colors[i];
+            } else {
+                squares[i].style.display = "none";
+                
+            }
         }
-    })
+    });
+    hardBtn.addEventListener("click", function(){
+        hardBtn.classList.add("selected");
+        easyBtn.classList.remove("selected");
+        numSquares = 6;
+        colors = generateRandomColors(numSquares);
+        pickedColor = pickColor();
+        colorDisplay.textContent = pickedColor;
+        for(var i = 0; i < squares.length; i++){
+            if(colors[i]){
+                squares[i].style.background = colors[i];
+            } else {
+                squares[i].style.display = "block";
+                
+            }
+        }
+    });
+
+    resetButton.addEventListener("click", function() {
+        reset();
+    });
+    
     colorDisplay.textContent = pickedColor;
-
-    for (var i = 0; i < squares.length; i++){
-        
-        // add initial colors to sqaures
-        squares[i].style.backgroundColor = colors[i];
-
-        // add event listeners to square
-        squares[i].addEventListener("click", function(){
-            //grab color of picked color
+    
+    for(var i = 0; i < squares.length; i++){
+        // add initial colors to squares
+        squares[i].style.background = colors[i];
+    
+        //add click listeners to squares
+        squares[i].addEventListener("click", function() {
+            //grab color of clicked squares
             var clickedColor = this.style.background;
-            // compare color to picked color
-            console.log(clickedColor,pickedColor);           
-            if(clickedColor === pickedColor){
+            //compare color to pickedColor
+            if(clickedColor === pickedColor) {
                 messageDisplay.textContent = "Correct!";
-                changeColors(clickedColor); 
-                h1.style.background =  clickedColor;     
+                resetButton.textContent = "Play Again?";
+                changeColors(clickedColor);
+                h1.style.background = clickedColor;
             } else {
                 this.style.background = "#232323";
-                messageDisplay.textContent = "Try Again."
+                messageDisplay.textContent = "Try Again";
             }
         });
     }
