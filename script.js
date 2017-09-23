0837675f7cccbd6a887b2491d9a7c21a3788d80c
egwuenu gift <laurandidi21@gmail.com>
2017-09-24 00:53:38 +0100

    error message display fixed :fire:

--- script.js
+++ script.js
@@ -45,6 +45,7 @@
         pickedColor = pickColor();
         //change colorDisplay to match picked Color
         colorDisplay.textContent = pickedColor;
+        this.textContent = "New Colors";
         //change colors of squares
         for(var i = 0; i < squares.length; i++) {
             squares[i].style.background = colors[i];
